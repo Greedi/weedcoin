40c74c8e60eb8ba99ca73e2f8fd54d5aa16597b4
Greedi <theGreedi@gmail.com>
2013-01-01 16:36:34 +0100

    genesis block remove from checkpoint

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -22,7 +22,8 @@ namespace Checkpoints
     //
     static MapCheckpoints mapCheckpoints =
         boost::assign::map_list_of
-        (     1, uint256("0x6021c4c42c1e8f80583a21128a59c2b5aeb70c716515c4b34e832ba5183f6731"))
+            (     1, uint256("0xe5973c14813833d5c482d6743433ff2e0b54f210cf4be8ae2e7044d7f66c06d4"))
+
         ;
 
     bool CheckBlock(int nHeight, const uint256& hash)
